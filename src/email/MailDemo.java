71d8b0fb9af8614b45e2d1afc65ab611c3590098
Benjamin Thorpe <benjaminthorpe19@gmail.com>
2021-02-07 16:38:35 +0000

    finished up the emailing code and it works

--- src/email/MailDemo.java
+++ src/email/MailDemo.java
@@ -3,6 +3,7 @@
 package email;
 
 import java.util.Properties;
+import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
@@ -17,45 +18,47 @@ public class MailDemo {
     public static void main(String[] args) throws Exception {
 
         String to = "benjaminthorpe19@gmail.com";
-
         String from = "marvelousbend@gmail.com";
 
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.starttls.enable", "true"); // can be optional
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465"); // 25 also works
+        Properties properties = new Properties();
+        // setting up the mail server
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.socketFactory.port", "465"); // optional
+        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        properties.put("mail.smtp.starttls.enable", "true"); // optional
+        properties.put("mail.smtp.auth", "true");
+        // port 25 also works, use port 587 when starting TLS (optional)
+        properties.put("mail.smtp.port", "465"); // optional
 
-        Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+        // Authenticating the user and password
+        Session session = Session.getDefaultInstance(properties, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(from, "marve!0us");
+                return new PasswordAuthentication(from, "password");
             }
         });
 
-        String msgBody = "Sending email using JavaMail API Test 3...";
+        String messageContent = "Sending email using JavaMail API Test 4...";
 
         try {
-            Message msg = new MimeMessage(session);
-            msg.setFrom(new InternetAddress(from, "NetBeans 12.0"));
-            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to, "Mr. Recipient"));
-            msg.setSubject("Java Mail API Test");
-            msg.setText(msgBody);
+            MimeMessage mimeMessage = new MimeMessage(session);
+            mimeMessage.setFrom(new InternetAddress(from, "NetBeans 12.0"));
+            mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(to, "Mr. Recipient"));
+            mimeMessage.setSubject("Java Mail API Test");
+            mimeMessage.setText(messageContent);
 
             System.out.println("Sending Email...");
-            Transport.send(msg);
+            Transport.send(mimeMessage);
             System.out.println("Email sent successfully...");
 
         } catch (AddressException e) {
-            System.out.println("Error 1: " + e.getMessage());
+            System.out.println("Error: " + e.getMessage());
         } catch (MessagingException e) {
-            System.out.println("Error 2: " + e.getLocalizedMessage());
-            System.out.println("Please Disable Less Secure App Access in your Google account");
-            System.out.println("Please put the correct username and password");
+            System.out.println("Error: " + e.getMessage());
+            System.out.println("Please Enable Less Secure App Access in your Google account.\n"
+                    + "Please put the correct username and password");
         } catch (Exception e) {
-            System.out.println("Error g: " + e.getMessage());
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
